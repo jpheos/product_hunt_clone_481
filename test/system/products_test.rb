633a30a6d4a73bf1e759e50c1b595f50d408005e
Joseph BLANCHARD <jooo.blanchard@gmail.com>
2020-11-27 09:51:18 +0100

    fix error and add first test

---
+++ test/system/products_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class ProductsTest < ApplicationSystemTestCase
+  test "visiting the root path" do
+    visit "/"
+    save_and_open_screenshot
+    assert_selector "h1", text: "Awesome Products"
+  end
+end
